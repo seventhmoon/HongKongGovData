252f247a1125ef5b6cdc602713d3359da888b08b
Fung <funglam@astri.org>
2017-03-13 15:18:04 +0800

    clean up

--- library/src/main/java/com/androidfung/opendata/ApiServices.java
+++ library/src/main/java/com/androidfung/opendata/ApiServices.java
@@ -13,15 +13,11 @@ import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
  * Created by fung on 3/4/2017.
  */
 
-public class ApiServices {
-
+public final class ApiServices {
 
     private static final String TRAMWAYS_API_BASE_URL = "https://www.com.hktramways.hktramways.com/";
-
     private static final String CLP_OPENDATA_API_BASE_URL = "https://opendata.clp.com.hk/";
-
     private static final String HK_GOV_DATA_API_BASE_URL = "https://api.data.gov.hk/v1/";
-
 
     public static GovDataApiService getGovDataApiService(OkHttpClient okHttpClient) {
         Retrofit retrofit = new Retrofit.Builder()
@@ -30,14 +26,11 @@ public class ApiServices {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit.create(GovDataApiService.class);
-
     }
 
 
     public static GovDataApiService getGovDataApiService() {
-
         return getGovDataApiService(new OkHttpClient());
-
     }
 
     public static ClpOpenDataApiService getClpOpenDataApiService(OkHttpClient okHttpClient) {
@@ -47,16 +40,12 @@ public class ApiServices {
                 .addConverterFactory(SimpleXmlConverterFactory.create())
                 .build();
         return retrofit.create(ClpOpenDataApiService.class);
-
     }
 
 
     public static ClpOpenDataApiService getClpOpenDataApiService() {
-
         return getClpOpenDataApiService(new OkHttpClient());
-
     }
-
 
     public static TramwaysApiService getTramwaysService(OkHttpClient okHttpClient) {
         Retrofit retrofit = new Retrofit.Builder()
@@ -65,15 +54,11 @@ public class ApiServices {
                 .addConverterFactory(SimpleXmlConverterFactory.create())
                 .build();
         return retrofit.create(TramwaysApiService.class);
-
     }
 
 
     public static TramwaysApiService getTramwaysService() {
-
         return getTramwaysService(new OkHttpClient());
-
     }
-
 
 }
